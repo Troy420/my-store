5aa2971e0585e16392f68685c3917cf191350224
Bernard <bernardy_ho@hotmail.com>
2019-07-16 20:47:31 +0800

    navbar done

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.1rem solid var(--cream);
+  color: var(--cream);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0; 
+  transition: all 0.3s ease-in-out;
+  &:hover{
+    background: var(--richBlack);
+  }
+  &:focus{
+    outline: none;
+  }
+`;
